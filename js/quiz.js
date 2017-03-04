35404e8dd1ffc87f1ec55d4154bc671176e39f75
Obieda01 <obieda01@email.franklin.edu>
2017-03-04 07:53:09 -0500

    add 03_01 branch

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,19 @@
-(function(){
+(function() {
 
-	// script goes here
+    var app = angular.module('myQuiz', [])
+    app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0;
+        $scope.persentage = 0;
+
+        $http.get('quiz_data.json').then(function(quizData) {
+            $scope.myQuestions = quizData.data;
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+
+
+    }]);
 
 })();
